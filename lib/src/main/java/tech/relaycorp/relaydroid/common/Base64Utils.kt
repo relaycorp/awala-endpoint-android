c8191236be606a0f2bae2de1d82a582257bb4394
Gus Narea <gnarea@users.noreply.github.com>
2021-03-10 10:44:34 +0000

    chore: Remove unused Base64Utils

--- lib/src/main/java/tech/relaycorp/relaydroid/common/Base64Utils.kt
+++
@@ -1,9 +0,0 @@
-package tech.relaycorp.relaydroid.common
-
-import android.util.Base64
-
-internal fun String.decodeBase64() =
-    Base64.decode(this, Base64.DEFAULT)
-
-internal fun ByteArray.encodeBase64() =
-    Base64.encode(this, Base64.DEFAULT)
