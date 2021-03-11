7bb0100ea76be46d3ad5f76714e9f12ac762f4e0
Sérgio Santos <me@sergiosantos.info>
2021-03-11 11:45:22 +0000

    Fix global vars

--- lib/src/main/java/tech/relaycorp/relaydroid/RelaynetTemp.kt
+++
@@ -1,6 +0,0 @@
-package tech.relaycorp.relaydroid
-
-// Temporary solution for the global val access issue
-public object RelaynetTemp {
-    public val GatewayClient: GatewayClientImpl get() = Relaynet.gatewayClientImpl
-}
