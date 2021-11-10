acbbf88815d33aca9b26402c7c4fc538f761b5a4
Gus Narea <gnarea@users.noreply.github.com>
2021-11-10 15:55:59 +0000

    delete unused TestAndroidProvider

--- lib/src/androidTest/java/tech/relaycorp/awaladroid/test/TestAndroidProvider.kt
+++
@@ -1,9 +0,0 @@
-package tech.relaycorp.awaladroid.test
-
-import android.content.Context
-import androidx.test.platform.app.InstrumentationRegistry
-
-internal object TestAndroidProvider {
-    val context: Context
-        get() = InstrumentationRegistry.getInstrumentation().targetContext
-}
