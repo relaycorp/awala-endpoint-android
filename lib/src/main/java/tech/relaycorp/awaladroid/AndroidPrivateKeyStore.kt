7db106e3d9c73f5c90c054fef36ff1245f378df9
Gus Narea <gnarea@users.noreply.github.com>
2021-11-10 15:45:01 +0000

    implement AwalaContext

---
+++ lib/src/main/java/tech/relaycorp/awaladroid/AndroidPrivateKeyStore.kt
@@ -0,0 +1,35 @@
+package tech.relaycorp.awaladroid
+
+import android.content.Context
+import androidx.security.crypto.EncryptedFile
+import androidx.security.crypto.MasterKey
+import java.io.File
+import tech.relaycorp.awala.keystores.file.FileKeystoreRoot
+import tech.relaycorp.awala.keystores.file.FilePrivateKeyStore
+
+internal class AndroidPrivateKeyStore(
+    root: FileKeystoreRoot,
+    private val context: Context
+) : FilePrivateKeyStore(root) {
+    override fun makeEncryptedInputStream(file: File) = buildEncryptedFile(file).openFileInput()
+
+    override fun makeEncryptedOutputStream(file: File) = buildEncryptedFile(file).openFileOutput()
+
+    private fun buildEncryptedFile(file: File) =
+        EncryptedFile.Builder(
+            context,
+            file,
+            masterKey,
+            EncryptedFile.FileEncryptionScheme.AES256_GCM_HKDF_4KB
+        ).build()
+
+    private val masterKey by lazy {
+        MasterKey.Builder(context, MASTER_KEY_ALIAS)
+            .setKeyScheme(MasterKey.KeyScheme.AES256_GCM)
+            .build()
+    }
+
+    companion object {
+        private const val MASTER_KEY_ALIAS = "_awaladroid_master_key_"
+    }
+}
--- lib/src/main/java/tech/relaycorp/awaladroid/Awala.kt
+++ lib/src/main/java/tech/relaycorp/awaladroid/Awala.kt
@@ -1,9 +1,13 @@
 package tech.relaycorp.awaladroid
 
 import android.content.Context
+import java.io.File
+import tech.relaycorp.awala.keystores.file.FileKeystoreRoot
+import tech.relaycorp.awala.keystores.file.FileSessionPublicKeystore
 import tech.relaycorp.awaladroid.background.ServiceInteractor
 import tech.relaycorp.awaladroid.storage.StorageImpl
 import tech.relaycorp.awaladroid.storage.persistence.EncryptedDiskPersistence
+import tech.relaycorp.relaynet.nodes.EndpointManager
 
 public object Awala {
     internal const val POWEB_PORT = 13276
@@ -21,15 +25,34 @@ public object Awala {
     /**
      * Set up the endpoint library.
      */
-    public suspend fun setup(context: Context) {
-        storage = StorageImpl(EncryptedDiskPersistence(context))
-        gatewayClientImpl = GatewayClientImpl(
-            serviceInteractorBuilder = { ServiceInteractor(context) }
+    public suspend fun setUp(context: Context) {
+        val keystoreRoot =
+            FileKeystoreRoot(File(context.filesDir, "awaladroid${File.separator}keystores"))
+        val androidPrivateKeyStore = AndroidPrivateKeyStore(keystoreRoot, context)
+        val fileSessionPublicKeystore = FileSessionPublicKeystore(keystoreRoot)
+        this.context = AwalaContext(
+            StorageImpl(EncryptedDiskPersistence(context)),
+            GatewayClientImpl(
+                serviceInteractorBuilder = { ServiceInteractor(context) }
+            ),
+            EndpointManager(androidPrivateKeyStore, fileSessionPublicKeystore),
+            androidPrivateKeyStore,
+            fileSessionPublicKeystore,
         )
+
+        storage = this.context!!.storage
+        gatewayClientImpl = this.context!!.gatewayClient
+    }
+
+    internal fun tearDown() {
+        context = null
     }
 
     internal lateinit var storage: StorageImpl
     internal var gatewayClientImpl: GatewayClientImpl? = null
+
+    private var context: AwalaContext? = null
+    internal fun getContext(): AwalaContext = context ?: throw SetupPendingException()
 }
 
 /**
