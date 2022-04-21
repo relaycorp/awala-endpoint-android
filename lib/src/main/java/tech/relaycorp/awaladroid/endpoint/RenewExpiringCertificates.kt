185bfb846a99ffdefd8178e832cd4220cf60844e
Sérgio Santos <me@sergiosantos.info>
2022-04-21 17:17:17 +0100

    feat: Renew expiring endpoint certificates (#223)
    Closes #198

---
+++ lib/src/main/java/tech/relaycorp/awaladroid/endpoint/RenewExpiringCertificates.kt
@@ -0,0 +1,31 @@
+package tech.relaycorp.awaladroid.endpoint
+
+import java.time.ZonedDateTime
+import kotlin.time.Duration
+import kotlin.time.days
+import tech.relaycorp.relaynet.keystores.PrivateKeyStore
+import tech.relaycorp.relaynet.wrappers.privateAddress
+import tech.relaycorp.relaynet.wrappers.x509.Certificate
+
+internal class RenewExpiringCertificates(
+    private val privateKeyStore: PrivateKeyStore,
+    private val firstPartyEndpointLoader: suspend (String) -> FirstPartyEndpoint?
+) {
+
+    suspend operator fun invoke() {
+        privateKeyStore.retrieveAllIdentityKeys()
+            .mapNotNull { firstPartyEndpointLoader(it.privateAddress) }
+            .forEach {
+                if (it.identityCertificate.isExpiring) {
+                    it.reRegister()
+                }
+            }
+    }
+
+    private val Certificate.isExpiring get() =
+        expiryDate <= ZonedDateTime.now().plusSeconds(EXPIRATION_THRESHOLD.inWholeSeconds)
+
+    companion object {
+        internal val EXPIRATION_THRESHOLD = Duration.days(60)
+    }
+}
