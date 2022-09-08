151d347904d1e93c1a905104addd3de9e15031eb
FilipeA <filiperamosalves@gmail.com>
2022-09-08 15:58:45 +0100

    fixed: issue with test

--- lib/src/test/java/tech/relaycorp/awaladroid/test/FirstPartyEndpointFactory.kt
+++ lib/src/test/java/tech/relaycorp/awaladroid/test/FirstPartyEndpointFactory.kt
@@ -9,6 +9,6 @@ internal object FirstPartyEndpointFactory {
         KeyPairSet.PRIVATE_ENDPOINT.private,
         PDACertPath.PRIVATE_ENDPOINT,
         listOf(PDACertPath.PRIVATE_GW),
-        null,
+        "frankfurt.relaycorp.cloud",
     )
 }
